bd5dd9b68a8a768553d362b9fde0c2ded78dea40
Lawrence John <thelaw111@gmail.com>
2024-03-20 08:50:16 +0100

    Added Migrations
    For database table creation and initial table setup

---
+++ migrations/tb_systemx_user.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: Systemx
+ * Date: 7/10/2020
+ * Time: 8:07 AM
+ */
+
+class tb_systemx_user {
+    public function up()
+    {
+        $db = \app\engine\Application::$app->db;
+        $SQL = "CREATE TABLE  tb_systemx_user(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                user_token VARCHAR(255) NOT NULL,
+                status TINYINT DEFAULT 0,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )  ENGINE=INNODB;";
+        $db->pdo->exec($SQL);
+    }
+
+    public function down()
+    {
+        $db = \app\engine\Application::$app->db;
+        $SQL = "DROP TABLE tb_systemx_user;";
+        $db->pdo->exec($SQL);
+    }
+}
